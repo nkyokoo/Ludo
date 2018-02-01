707067045b521d029deb48bee9b5ead8849de4b0
kyoko <notorious.droid@gmail.com>
2018-02-01 13:55:37 +0100

    trying to make turns work aaaaaaaaaaaaaa

--- Assets/scripts/game/dice/Dice.cs
+++ Assets/scripts/game/dice/Dice.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Assets.scripts.game.dice;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Dice : DieTurn {
 	public GameObject gameObject = null;
-
+	public static DiceStates state;
 	public void RollDice ()
 	{
+		
 		System.Random r = new System.Random();
 		int rInt = r.Next(1, 7);
 		this.changeSide(rInt, gameObject);
-		Debug.Log(rInt);
-		
-
+		state = DiceStates.Rolled;
 	}
 	
 
---
+++ Assets/scripts/game/player/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.scripts.game.dice;
+using game.player;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	
+	private PlayerTurns currentTurn;
+	public GameObject gameObject;
+
+	private Text txt;
+	public GameObject myButton;
+
+	
+	// Use this for initialization
+
+	void Start ()
+	{
+	  txt = gameObject.GetComponent<Text>();
+
+		currentTurn = PlayerTurns.PLAYER1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		switch (currentTurn)
+		{
+			case PlayerTurns.PLAYER1:
+				txt.text = currentTurn.ToString().ToLower() + "'s turn";
+				if (Dice.state == DiceStates.Rolled)
+				{
+					myButton.SetActive(false);
+				}
+				
+				
+				break;
+			case PlayerTurns.PLAYER2:
+				txt.text = currentTurn.ToString().ToLower() + "'s turn";
+				if (Dice.state == DiceStates.Rolled)
+				{
+					myButton.SetActive(false);
+				}
+			
+				
+				break;
+			case PlayerTurns.PLAYER3:
+				txt.text = currentTurn.ToString().ToLower() + "'s turn";
+				if (Dice.state == DiceStates.Rolled)
+				{
+					myButton.SetActive(false);
+				}
+				
+				
+				break;
+			case PlayerTurns.PLAYER4:
+				txt.text = currentTurn.ToString().ToLower() + "'s turn";
+				if (Dice.state == DiceStates.Rolled)
+				{
+					myButton.SetActive(false);
+				}
+	
+				
+				break;
+		}
+		
+	}
+
+	public void nextTurn()
+	{
+		foreach (PlayerTurns turns in Enum.GetValues(typeof(PlayerTurns)))
+		{
+			if (currentTurn == turns)
+			{
+				currentTurn = turns + 1;
+			
+			}
+			else
+			{
+				currentTurn = turns;
+				break;
+			}
+			
+			
+		
+		}
+	}
+}
